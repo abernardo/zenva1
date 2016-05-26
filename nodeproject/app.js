54581ddb82453ef1bd386da408e20125d5ea7673
Alexandre <alexandre.m.bernardo@gmail.com>
2016-05-26 23:17:50 +0000

    section 6

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,17 @@
-var aloha = 'boda';
-console.log(aloha);
+var express = require("express");
+var app = express();
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+app.listen(port,function(err){
+    console.log('the server is running on port: '+port);
+});
+app.get('/',function(req,res){
+    res.send('<h3>Boda in the house</h3>');
+});
+app.get('/routing',function(req,res){
+    res.send('<h3>Boda in the routing</h3>');
+});
